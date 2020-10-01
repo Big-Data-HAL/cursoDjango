cb6fe65c5d55cdbe9b4ad6b52d6bd00284b593cf
Rozelio Romao <rozelio.romao@gmail.com>
2020-10-01 17:28:22 -0300

    Adicionado ao projeto ProjBiblioteca no app biblioteca a model.py

--- djangoProjeto/projBiblioteca/biblioteca/models.py
+++ djangoProjeto/projBiblioteca/biblioteca/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Livro(models.Model):
+    autor = models.CharField(max_length=50)
+    editora = models.CharField(max_length=50)
+    isbn = models.IntegerField(default=0)
+    titulo = models.CharField(max_length=50)
+    anoPublicacao = models.IntegerField(default=0)
+    emailEditora = models.EmailField(max_length=50)
