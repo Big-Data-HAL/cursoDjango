66486b7c4780d779e0c713842d7764423e6d932e
Rozelio Romao <rozelio.romao@gmail.com>
2020-10-16 17:00:45 -0300

    Inclusão da funcionalidade cadastrar (com falha - TypeError: context must be a dict rather than set.

--- djangoProjeto/projBiblioteca/biblioteca/models.py
+++ djangoProjeto/projBiblioteca/biblioteca/models.py
@@ -5,6 +5,7 @@ class Livro(models.Model):
     autor = models.CharField(max_length=50)
     editora = models.CharField(max_length=50)
     isbn = models.IntegerField(default=0)
+    numeroPaginas = models.IntegerField(default=0)
     titulo = models.CharField(max_length=50)
     anoPublicacao = models.IntegerField(default=0)
     emailEditora = models.EmailField(max_length=50)
