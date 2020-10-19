5701c6aa8553a1ce69e16dd8ecee6e219a2d215f
Rozelio Romao <rozelio.romao@gmail.com>
2020-10-19 15:50:52 -0300

    Criada a função de edição de livros

--- djangoProjeto/projBiblioteca/biblioteca/urls.py
+++ djangoProjeto/projBiblioteca/biblioteca/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^livro_list/', livro_list, name='livro_list'),
-    url(r'livro_new/',livro_new, name='livro_new')
+    url(r'livro_new/',livro_new, name='livro_new'),
+    url(r'livro_edit/,(?P<pk>[0-9]+)', livro_edit, name='livro_edit')
 ]
