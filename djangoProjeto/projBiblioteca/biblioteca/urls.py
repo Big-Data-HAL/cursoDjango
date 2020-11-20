90768bcb9566de8957e7e23b8302bad97b4c33c7
Rozelio Romao <rozelio.romao@gmail.com>
2020-11-20 18:52:34 -0300

    Finalizada a funcionalidade de executar o lista livro pelo admin.

--- djangoProjeto/projBiblioteca/biblioteca/urls.py
+++ djangoProjeto/projBiblioteca/biblioteca/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^livro_list/', livro_list, name='livro_list'),
+    url(r'^admin/livro_list/', livro_list, name='livro_list'),
     url('home/', home, name="home"),
     url(r'^livro_new/',livro_new, name='livro_new'),
     url(r'^livro_edit/,(?P<pk>[0-9]+)', livro_edit, name='livro_edit'),
