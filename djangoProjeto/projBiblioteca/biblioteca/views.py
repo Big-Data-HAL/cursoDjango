217d49414302c11e1bc9ce994d27a0f071f018e7
glauber.romao <rocharomao@gmail.com>
2020-11-28 10:43:52 -0300

    Novo app mlteca

--- djangoProjeto/projBiblioteca/biblioteca/views.py
+++ djangoProjeto/projBiblioteca/biblioteca/views.py
@@ -4,35 +4,36 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 import datetime
 from .bo import calculadora
-
+from .bo import mlteca
 
 # Create your views here.
 
 def home(request, template_name='home_.html'):
     vnow = datetime.datetime.now()
     dnow = {'vnow': vnow}
-    # html = "<html><body>It is now %s.</body></html>" %now
-    # return HttpResponse(html)
-    return render(request, template_name, dnow)
-
+    #html = "<html><body>It is now %s.</body></html>" %now
+    #return HttpResponse(html)
+    return render(request, template_name,dnow)
 
 class LivroForm(ModelForm):
     class Meta:
         model = Livro
-        fields = ['autor', 'editora', 'isbn', 'numeroPaginas', 'titulo', 'anoPublicacao', 'emailEditora']
+        fields = ['autor', 'editora', 'isbn', 'numeroPaginas', 'titulo', 'anoPublicacao','emailEditora']
 
+class MlInput(ModelForm):
+    class Input:
+        model = MlTeca
+        fields = ['paginas', 'publicacao', 'assunto', 'avaliacao']
+
+# def livro_list(request, template_name= 'livro_list.html'):
+    # livro = Livro.objects.all
+    # livros = {'lista:', livro}
+    #return render(request, template_name, livros)
 
 def livro_list(request, template_name='livro_list.html'):
     livro = Livro.objects.all()
     livros = {'lista': livro}
     return render(request, template_name, livros)
-
-
-# def livro_busca(request, template_name='livro_busca.html'):
-    # livro = Livro.objects.filter()
-    # vbusca = {'lista': livro}
-    # return render(request, template_name, vbusca)
-
 
 def livro_new(request, template_name='livro_form.html'):
     form = LivroForm(request.POST or None)
@@ -53,7 +54,6 @@ def livro_edit(request, pk, template_name='livro_form.html'):
         form = LivroForm(instance=livro)
     return render(request, template_name, {'form': form})
 
-
 def livro_remove(request, pk):
     livro = Livro.objects.get(pk=pk)
     if request.method == "POST":
@@ -61,10 +61,14 @@ def livro_remove(request, pk):
         return redirect('livro_list')
     return render(request, 'livro_delete.html', {'livro': livro})
 
-
 def vbo(request):
-    res2 = calculadora.somar(3, 5)
+    res2 = calculadora.somar(3,5)
     fig = calculadora.grafico()
-    # html = calculadora.plotly()
+    #html = calculadora.plotly()
     dnow = {'vnow': fig, 'vnow2': fig}
     return render(request, 'home_.html', dnow)
+
+def mlteca_input(request):
+    form = MlInput(request.POST or None)
+    #mlteca(form)
+    return render(request, 'livro_mlteca.html', {'form': form})
