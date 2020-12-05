4f9121487364b5be17d6f79653ad36046d6ae815
glauber.romao <rocharomao@gmail.com>
2020-12-05 11:02:38 -0300

    teste de Home

--- djangoProjeto/projBiblioteca/biblioteca/urls.py
+++ djangoProjeto/projBiblioteca/biblioteca/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     url(r'^livro_list/', livro_list, name='livro_list'),
-    url('home/', home, name="home"),
     url(r'^livro_new/',livro_new, name='livro_new'),
     url(r'^livro_edit/,(?P<pk>[0-9]+)', livro_edit, name='livro_edit'),
     url(r'^livro_remove/,(?P<pk>[0-9]+)', livro_remove, name='livro_remove'),
     url('calc/', vbo, name="cal"),
+    url(r'home/', home, name="home"),
     #url('mlteca/', mlteca_input, name="mlteca_input")
 
 ]
