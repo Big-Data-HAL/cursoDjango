6ce20f69325b7cc3dfe1f6b1d870f9835d4cbc59
glauber.romao <rocharomao@gmail.com>
2020-12-05 10:19:31 -0300

    search alinhado

--- djangoProjeto/projBiblioteca/biblioteca/urls.py
+++ djangoProjeto/projBiblioteca/biblioteca/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^livro_edit/,(?P<pk>[0-9]+)', livro_edit, name='livro_edit'),
     url(r'^livro_remove/,(?P<pk>[0-9]+)', livro_remove, name='livro_remove'),
     url('calc/', vbo, name="cal"),
-    url('mlteca/', mlteca_input, name="mlteca_input")
+    #url('mlteca/', mlteca_input, name="mlteca_input")
 
 ]
