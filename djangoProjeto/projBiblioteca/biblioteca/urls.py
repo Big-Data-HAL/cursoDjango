80b251473c7bb5b465107f2fb7a5f01367defa24
glauber.romao <rocharomao@gmail.com>
2020-12-12 10:58:09 -0300

    Atualização da URL para Home como página inicial.

--- djangoProjeto/projBiblioteca/biblioteca/urls.py
+++ djangoProjeto/projBiblioteca/biblioteca/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^livro_edit/,(?P<pk>[0-9]+)', livro_edit, name='livro_edit'),
     url(r'^livro_remove/,(?P<pk>[0-9]+)', livro_remove, name='livro_remove'),
     url('calc/', vbo, name="cal"),
-    url(r'home/', home, name="home"),
+    url(r'', home, name="home"),
     #url('mlteca/', mlteca_input, name="mlteca_input")
 
 ]
